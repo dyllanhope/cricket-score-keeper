80c925e6ed708ad031c98dd088ca6b067e801f28
Dyllan Hope <dyllanjhope@gmail.com>
2019-09-20 14:54:37 +0200

    cant set negative over amount

--- cricket-score-keeper.js
+++ cricket-score-keeper.js
@@ -15,29 +15,29 @@ module.exports = () => {
             for (let i = 0; i < over.length; i++) {
                 if (wickets !== 10) {
                     switch (over[i]) {
-                    case '1':
-                        score += 1;
-                        break;
-                    case '2':
-                        score += 2;
-                        break;
-                    case '3':
-                        score += 3;
-                        break;
-                    case '4':
-                        score += 4;
-                        break;
-                    case '6':
-                        score += 6;
-                        break;
-                    case 'w':
-                        wickets++;
-                        if (wickets === 10) {
-                            gameDone = true;
-                        }
-                        break;
-                    default:
-                        score += 0;
+                        case '1':
+                            score += 1;
+                            break;
+                        case '2':
+                            score += 2;
+                            break;
+                        case '3':
+                            score += 3;
+                            break;
+                        case '4':
+                            score += 4;
+                            break;
+                        case '6':
+                            score += 6;
+                            break;
+                        case 'w':
+                            wickets++;
+                            if (wickets === 10) {
+                                gameDone = true;
+                            }
+                            break;
+                        default:
+                            score += 0;
                     };
                 };
             };
@@ -54,8 +54,10 @@ module.exports = () => {
     const wicketCount = () => { return wickets; };
 
     const setOvers = (num) => {
-        overSet = true;
-        overs = Number(num);
+        if (num > 0) {
+            overSet = true;
+            overs = Number(num);
+        }
     };
 
     const overCount = () => {
@@ -93,27 +95,27 @@ module.exports = () => {
         for (let i = 0; i < 6; i++) {
             const randomNum = Math.floor(Math.random() * 7) + 1;
             switch (randomNum) {
-            case 1:
-                currentOver += '1';
-                break;
-            case 2:
-                currentOver += '2';
-                break;
-            case 3:
-                currentOver += '3';
-                break;
-            case 4:
-                currentOver += '4';
-                break;
-            case 5:
-                currentOver += '6';
-                break;
-            case 6:
-                currentOver += 'w';
-                break;
-            case 7:
-                currentOver += '-';
-                break;
+                case 1:
+                    currentOver += '1';
+                    break;
+                case 2:
+                    currentOver += '2';
+                    break;
+                case 3:
+                    currentOver += '3';
+                    break;
+                case 4:
+                    currentOver += '4';
+                    break;
+                case 5:
+                    currentOver += '6';
+                    break;
+                case 6:
+                    currentOver += 'w';
+                    break;
+                case 7:
+                    currentOver += '-';
+                    break;
             }
         }
     };
